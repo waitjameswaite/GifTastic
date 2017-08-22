2056075d4b05cc6e50c10df6e0cd31156bc5a686
James Waite <James Waite>
2017-08-22 18:37:17 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	        
 	        var topicsBtn = $("<button>");
 	        
-	        topicsBtn.addClass("btn btn-link pull-right");
+	        topicsBtn.addClass("btn btn-link pull-right gifTopic");
 	        
 	        topicsBtn.attr("data-topic", topics[i]);
 
@@ -19,16 +19,49 @@ $(document).ready(function(){
 	        $("#btnContainer").append(topicsBtn);
 	    }
 
-	    $("#id").on("click", function(event) {
+	    $("#addTopicBtn").on("click", function(event) {
 
 	    	event.preventDefault();
 
-	    	var userTopic = $("#textBox").val().trim();
+	    	var userTopic = $("#userInput").val().trim();
 	    	topics.push(userTopic);
 
 	    	renderBtns();
 	    });
 	}
+
+	$(".gifTopic").on("click", function() {
+
+		var gif = $(this).attr("data-topic");
+
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        gif + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+        	url: queryURL,
+        	method: "GET"
+        })
+        .done(function(response) {
+
+        	var results = response.data;
+
+        	for (var i = 0; i < results.length; i++) {
+        		if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+        			var gifDiv = $("<div class='item'>");
+        			var rating = results[i].rating;
+        			var p = $("<p>").text("Rating: " + rating);
+        			var gifImage = $("<img>");
+        			gifImage.attr("src", results[i].images.fixed_height.url);
+        			gifDiv.append(p);
+        			gifDiv.append(gifImage);
+
+        			$("#gifs-appear-here").prepend(gifDiv);
+        		}
+        	}
+
+        })
+
+	});
 
 	renderBtns();
 });
